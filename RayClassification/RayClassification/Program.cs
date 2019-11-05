4b3e092fdb171f85885d06ffaa40d25afb82e00a
ShimuSourav <rdfox.007@gmail.com>
2019-11-05 23:46:52 +0600

    newly added

---
+++ RayClassification/RayClassification/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alpha");
+            Console.WriteLine("Beta");
+            Console.WriteLine("Gama");
+
+            Console.ReadKey();
+        }
+    }
+}
